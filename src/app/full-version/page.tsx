867957e5987bfbfe60066dcdd88c0a2a22f7d99b
fvrlq <mfariq.122400086@student.itera.ac.id>
2026-07-02 03:37:49 +0700

    delete folder

--- src/app/full-version/page.tsx
+++
@@ -1,9 +0,0 @@
-import JourneySection from "@/components/sections/home/journeysection";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <FullVersionSection />
-    </main>
-  );
-}
